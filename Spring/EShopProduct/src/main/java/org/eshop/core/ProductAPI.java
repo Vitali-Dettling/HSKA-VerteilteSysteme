f186614ebde30a63c622e177361dba414a894449
vislab <christian.zirpins@hs-karlsruhe.de>
2015-11-26 15:45:53 +0100

    Created spring project

---
+++ Spring/EShopProduct/src/main/java/org/eshop/core/ProductAPI.java
@@ -0,0 +1,28 @@
+package org.eshop.core;
+
+public class ProductAPI {
+	
+	private String category;
+	private String product;
+	
+	
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+
+	
+	
+	
+
+}
