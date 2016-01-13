9ae78aa251ea224addadae28a315e2171b5ed2e2
Sebastian <sebastian.t.schork@gmail.com>
2016-01-13 12:02:42 +0100

    added EUREKA basis

--- Spring/Account/src/main/java/org/eshop/account/AccountApplication.java
+++ Spring/Account/src/main/java/org/eshop/account/AccountApplication.java
@@ -2,10 +2,12 @@ package org.eshop.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableDiscoveryClient
 public class AccountApplication {
 
 	public static void main(String[] args) {
